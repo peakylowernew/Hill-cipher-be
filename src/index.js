9d89fc9a1655e89f33b55cab05b050e258f1bc46
vinh <peakylower@gmail.com>
2025-04-17 17:19:28 +0700

    luu code

--- src/index.js
+++ src/index.js
@@ -30,5 +30,5 @@ app.post("/api/hill/decrypt", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

fe4b6710ee7b6e93e74a6e1312b6a92011a92f33
vinh <peakylower@gmail.com>
2025-05-08 10:54:03 +0700

    update login google

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, signup, getUserById } from "../controllers/authController.js";
+import { login, signup, getUserById, googleLogin } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", signup);
 router.get("/user/:uid", getUserById); //đẻ tạm
+router.post("/google", googleLogin);
 
 export default router;
